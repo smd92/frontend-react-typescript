9d46a631495e07df91ec8dda8ba962ce664320ce
smd92 <69214590+smd92@users.noreply.github.com>
2024-09-25 20:33:41 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 const App = () => {
   return (
-    <div className="darkBlue">
-      <h1 className="text-3xl font-bold underline text-background">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline text-card">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline text-text">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline text-button">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline text-button-hover">Hello world!</h1>
-    </div>
+    <>
+      <div className="darkBlue">
+        <h1 className="text-3xl font-bold underline text-background">
+          Hello world!
+        </h1>
+        <h1 className="text-3xl font-bold underline text-card">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-text">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-button">
+          Hello world!
+        </h1>
+        <h1 className="text-3xl font-bold underline text-button-hover">
+          Hello world!
+        </h1>
+      </div>
+      <div className="darkGreen">
+        <h1 className="text-3xl font-bold underline text-background">
+          Hello world!
+        </h1>
+        <h1 className="text-3xl font-bold underline text-card">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-text">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-button">
+          Hello world!
+        </h1>
+        <h1 className="text-3xl font-bold underline text-button-hover">
+          Hello world!
+        </h1>
+      </div>
+    </>
   );
 };
 
