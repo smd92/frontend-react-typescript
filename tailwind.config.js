1da244e41c2d111c76152e7eeedff129862af486
smd92 <69214590+smd92@users.noreply.github.com>
2024-09-25 16:51:35 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      "gray-dark": "#333333",
+      "white-gray": "#F5F5F5",
+      "petrol-blue": "#00796B",
+      pink: "#E91E663",
+    },
     extend: {},
   },
   plugins: [],
