df0bc15d9a79a8d85f13682ddccedabdf714503e
smd92 <sassan.marouf@gmail.com>
2024-09-28 21:42:56 +0200

    :

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,9 @@
+const themes = ["swiss", "neon"];
+
+const ThemeSwitcher = () => {
+    return (
+        <div>ThemeSwitcher</div>
+    )
+}
+
+export default ThemeSwitcher;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
 const { text } = require("stream/consumers");
 const withMT = require("@material-tailwind/react/utils/withMT");
 
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      textColor: {
+        skin: {
+          base: withOpacity('--color-text-base'),
+          muted: withOpacity('--color-text-muted'),
+          inverted: withOpacity('--color-text-inverted'),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity('--color-fill'),
+          'button-accent': withOpacity('--color-button-accent'),
+          'button-accent-hover': withOpacity('--color-button-accent-hover'),
+          'button-muted': withOpacity('--color-button-muted'),
+        },
+      },
+      gradientColorStops: {
+        skin: {
+          hue: withOpacity('--color-fill'),
+        },
+      },
       fontFamily: {
         "concert-one": ["Concert One", "sans-serif"],
       }
